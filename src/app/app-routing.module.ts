5e242340c42f43437ff90ae85e0daf1c802c24e3
Indrajeet Mandloi <imandloi@ad.bluemsp.com>
2018-10-17 23:09:26 -0500

    set up project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'part-one', loadChildren: './part-one/part-one.module#PartOnePageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/part-one/part-one.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-part-one',
+  templateUrl: './part-one.page.html',
+  styleUrls: ['./part-one.page.scss'],
+})
+export class PartOnePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
